ef8f0bb569a3e5253d81e8ed426b014801850f88
blameleo <leonardgre@gmail.com>
2023-01-26 19:18:11 +0000

    added custom font to app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Navbar from "./Navbar"
+
+function App() {
+  return (
+    <div className="App bg-gray-800 text-white font-custom">
+
+
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav>
+        <div>
+            <h1 className="capitalize">dental pro clinic</h1>
+        </div>
+      </nav>
+    </div>
+  );
+}
